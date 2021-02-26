1a63ac3af78b3c1d03307034a4c37426bcd6e8e2
Dora Budić <dbudic0706@gmail.com>
2021-02-26 15:14:06 +0100

    Finished

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // Ask for name
 var name = prompt("Write your name");
+
+// Ask for surname
+var surname = prompt("Write your surname");
+
+// Ask for favourite color
+var color = prompt("What is your favourite color?")
+
+// Write password made of these 3 info
+document.getElementById('password').innerHTML = "Your password is: " + name + surname + color + "21";
