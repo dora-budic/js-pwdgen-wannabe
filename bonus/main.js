e4812b7b9990d67ba76b06d466103ef972cba2af
Dora Budić <dbudic0706@gmail.com>
2021-02-26 17:52:32 +0100

    Added more questions in js

--- bonus/main.js
+++ bonus/main.js
@@ -1,8 +1,14 @@
 // Ask for name
-var name = prompt("Write your name");
+var name = prompt("What is your name?");
 
 // Ask for surname
-var surname = prompt("Write your surname");
+var surname = prompt("What is your surname");
+
+// Ask for age
+var age = prompt("How old are you?");
+
+// Ask for city
+var city = prompt("Write the name of your hometown");
 
 // Ask for favourite color
 var color = prompt("What is your favourite color?")
@@ -10,5 +16,11 @@ var color = prompt("What is your favourite color?")
 // Say hello
 document.getElementById('greeting').innerHTML = "Hello " + name + "!";
 
-// Write password made of these 3 info
+// info
+document.getElementById('name').innerHTML = name + " " + surname;
+document.getElementById('age').innerHTML = age;
+document.getElementById('city').innerHTML = city;
+document.getElementById('color').innerHTML = color;
+
+// Write password made of 3 info
 document.getElementById('password').innerHTML = name + surname + color + "21";
